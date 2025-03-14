b3707e64ea347e5b9b3e0f41db9722b343b5b193
Petrik Diak <diak@petrik.hu>
2025-03-14 12:24:51 +0100

    Kesz

---
+++ Motors/Motor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motors
+{
+	internal class Motor
+	{
+		string brand;
+		string name;
+		int releaseYear;
+		double performance;
+		double priceInEur;
+
+		public Motor(string brand, string name, int releaseYear, double performance, double priceInEur)
+		{
+			this.brand = brand;
+			this.name = name;
+			this.releaseYear = releaseYear;
+			this.performance = performance;
+			this.priceInEur = priceInEur;
+		}
+
+		public string Brand { get => brand; set => brand = value; }
+		public string Name { get => name; set => name = value; }
+		public int ReleaseYear { get => releaseYear; set => releaseYear = value; }
+		public double Performance { get => performance; set => performance = value; }
+		public double PriceInEur { get => priceInEur; set => priceInEur = value; }
+
+		public override string ToString()
+		{
+			return $"{Name} ({Brand}), {ReleaseYear}, {Performance} HP, {PriceInEur} EUR";
+		}
+	}
+}
--- Motors/Program.cs
+++ Motors/Program.cs
@@ -4,7 +4,15 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
-		}
+			Statisztika statisztika = new Statisztika();
+			statisztika.ReadFromFile("C:\\Users\\Diák\\source\\repos\\Motors\\Motors\\bin\\Debug\\net8.0\\motors.txt");
+
+			Console.WriteLine("SumPrices: " + statisztika.SumPrices());
+			Console.WriteLine("Contains: " + statisztika.Contains("Yamaha YZF-R1"));
+			Console.WriteLine("Oldest" + statisztika.Oldest());
+			Console.WriteLine("SumBrand: " + statisztika.SumBasedOnBrand("Yamaha"));
+			statisztika.Sort();
+
+        }
 	}
 }
